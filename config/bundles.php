6c2280343d3d6e7f0d38f1d826a9153947ed173c
Albert <adalbert.iceberg@gmail.com>
2019-05-19 15:15:06 +0300

    JMSSerializer added

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     OldSound\RabbitMqBundle\OldSoundRabbitMqBundle::class => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
 ];
